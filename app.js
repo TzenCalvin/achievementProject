3a25d2b012f93f444909b65bae8226586055c6bb
nivlac <nivlac@uw.edu>
2024-06-22 13:11:56 -0700

    Set up framework

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const multer = require('multer');
+
+app.use(multer().none());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const SERVER_SIDE_ERROR_MSG = 'Oh no! An error occurred on the server. Try again later.';
+const SERVER_SIDE_ERROR_STATUS_CODE = 500;
+const CLIENT_SIDE_ERROR_STATUS_CODE = 400;
+
+app.use(express.static('public'));
+const PORT = process.env.PORT || 8001;
+app.listen(PORT);
---
+++ public/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+(function() {
+
+  // HELPER FUNCTIONS
+
+  /**
+   * Shortcut that returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID.
+   * @return {object} - DOM object associated with id.
+   */
+  function id(id) {
+    return document.getElementById(id);
+  }
+
+  /**
+   * Shortcut that returns the first element matching the specified selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - First DOM object associated with selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Shortcut that returns an array of elements matching the given query.
+   * @param {string} selector - CSS query selector.
+   * @returns {array} - Array of DOM objects matching the given query.
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+})();
